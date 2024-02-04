baf857a1dec0b9da198f18d1b9cd2cab8a816eeb
BryanCurillo <bryancurillo2002@gmail.com>
2024-02-04 17:24:20 -0500

    Portafolio CV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,36 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portafolio';
+  correoElectronico = 'bryancurillo2002@gmail.com';
+  imgPerfil = '/src/assets/Bryan.png';
+
+  enviarCorreo() {
+    window.location.href = `mailto:${this.correoElectronico}`;
+  }
+
+  abrirCV() {
+    window.open('https://drive.google.com/file/d/1U9wzmMbEaj7GHAYkquLjaedTMCFv8q43/view?usp=sharing', '_blank');
+  }
+
+  abrirGitHub() {
+    window.open('https://github.com/bacurillo?tab=repositories', '_blank');
+  }
+
+  abrirLinkedIn() {
+    window.open('https://www.linkedin.com/in/bryan-curillo-945253171/', '_blank');
+  }
+
+  gh1() {
+    window.open('https://github.com/bacurillo/PreProfecionales_Senescyt', '_blank');
+  }
+  gh2() {
+    window.open('https://github.com/bacurillo/Vinculacion_Fundacion_Alianza', '_blank');
+  }
+  gh3() {
+    window.open('https://github.com/bacurillo/ProyectoZoo', '_blank');
+  }
+  gh4() {
+    window.open('https://github.com/bacurillo/Alquiler_Salones', '_blank');
+  }
+
 }
