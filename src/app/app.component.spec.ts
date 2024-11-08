bd3feacd3a8d06ac742c9aec928efa1bf5b19b1d
BryanCurillo <bryancurillo2002@gmail.com>
2024-11-07 23:13:57 -0500

    prueba

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
+//ayuda
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
